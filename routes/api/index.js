e3bebb53c036409b897bfed963bcfb9a4ce3bfa6
khaoula <khaoulajabour4@gmail.com>
2022-02-16 19:32:22 -0500

    add comment methods and routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const pizzaRoutes = require('./pizza-routes');
+const commentRoutes = require('./comment-routes');
 
 
+router.use('/comments', commentRoutes);
 router.use('/pizzas', pizzaRoutes);
 
 
