1fc75a0ad8e361ebbd7f825078f380dc2af803fc
khaoula <khaoulajabour4@gmail.com>
2022-02-19 03:11:02 -0500

    set up replies

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const { addComment, removeComment, addRepy, removeReply } = require('../../controllers/comment-controller');
 
 
 router.route('/:pizzaId').post(addComment);
 
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router.route('/:pizzaId/:commentId')
+.put(addRepy)
+.delete(removeComment);
+
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 
  
 module.exports = router;
