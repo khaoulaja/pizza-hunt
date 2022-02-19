0aa2e8bec894cf4336e8cdd411979e8052eeca93
khaoula <khaoulajabour4@gmail.com>
2022-02-19 04:47:11 -0500

    add offline persistence and validation

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -46,7 +46,7 @@ const pizzaController = {
     //Update pizza by id
     updatePizza({params, body}, res){
         // by setting that third parameter, { new: true }, it will return the new version of the document.
-        Pizza.findOneAndUpdate({_id: params.id}, body, {new: true})
+        Pizza.findOneAndUpdate({_id: params.id}, body, {new: true, runValidators: true})
         .then(dbPizzaData => {
             if(!dbPizzaData){
                 res.status(404).json({message: 'No pizza found with this id!'});
